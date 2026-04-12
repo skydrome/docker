a6bf453005a7094078a2966078d0ca264d801b7b
skydrome <skydrome@protonmail.com>
2026-04-12 15:37:55 -0400

    think about python venv

--- eggdrop/entrypoint.sh
+++ eggdrop/entrypoint.sh
@@ -1,26 +1,40 @@
 #!/bin/sh
+# shellcheck source=/dev/null
 set -e
 
 # allow entering
 [ "$1" = 'sh' ] && exec /bin/bash
 
-mkdir -p /eggdrop-data/scripts
-chown -R eggdrop /eggdrop-data
-su-exec eggdrop ln -sf /eggdrop-data/scripts /eggdrop/scripts
+# start as root to set permissions, then drop to unprivileged user
+if [ "$(id -u)" = "0" ]; then
+    dropbear -Ems \
+    -r /etc/dropbear/dropbear_ed25519_host_key \
+    -p 0.0.0.0:22 &&
+    echo "SSH started on port 22" || echo "SSH failed to start"
+
+    mkdir -p /eggdrop-data/scripts
+    chown -R eggdrop /eggdrop-data
+    su-exec eggdrop "$0" "$@"
+    exit 0
+fi
+
+cd /eggdrop
+
+ln -sf /eggdrop-data/scripts ./scripts
 
 [ -f '/eggdrop-data/eggdrop.conf' ] || {
     echo "cant read eggdrop.conf"
     exit 1
 }
 
-cd /eggdrop ||exit 1
+# if [ -f '/eggdrop-data/.venv/bin/activate' ]; then
+#     echo "==> Activating python venv"
+#     . .venv/bin/activate
+# else
+#     echo "==> Creating python venv"
+#     uv venv /eggdrop-data/.venv
+# fi
 
-dropbear -Ems \
-    -r /etc/dropbear/dropbear_ed25519_host_key \
-    -p 0.0.0.0:22 &&
-    echo "SSH started on port 22" || echo "SSH failed to start"
-
-echo "==> Starting eggdrop with $(id eggdrop)"
+echo "==> Starting eggdrop with uid: $(id eggdrop)"
 LD_PRELOAD=/usr/lib/libmimalloc-secure.so.2 \
-su-exec eggdrop \
-    ./eggdrop /eggdrop-data/eggdrop.conf && tail -qF /tmp/console.log
+./eggdrop -t /eggdrop-data/eggdrop.conf  #&& tail -qF /tmp/console.log
--- eggdrop/help.sh
+++ eggdrop/help.sh
@@ -4,5 +4,5 @@ echo "
 docker buildx build . --progress=plain --platform=linux/arm/v7 -t skydrome/eggdrop -t skydrome/eggdrop:armv7
     --load | --push | --no-cache
 
-docker run --rm -it --platform=linux/arm/v7 -v data:/eggdrop-data skydrome/eggdrop:${1:-latest} sh
+docker run --rm -it --platform=linux/arm/v7 -v ./data:/eggdrop-data skydrome/eggdrop:${1:-latest} sh
 "
